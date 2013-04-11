6d7fdcff40948767d16f513fe158e932983c1021
Alexey Badenkov <alexey.badenkov@gmail.com>
2013-04-11 16:44:08 +0400

    В процессе рефакторинга

--- app/controllers/web/users_controller.rb
+++ app/controllers/web/users_controller.rb
@@ -6,7 +6,8 @@ class Web::UsersController < Web::ApplicationController
   def create
     @type = UserRegistrationType.new(params[:user])
     if @type.save
-      redirect_to new_session_path
+      sign_in(@type)
+      redirect_to root_path
     else
       render action: 'new'
     end
